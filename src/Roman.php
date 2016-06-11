f11afc7e817355793f7d5d87643dd61aadf74a6e
Enrique Barbeito García <ebarbeito@gmail.com>
2016-06-11 12:53:20 +0200

    Remove test 50 should be L, I don't want to see it for now

--- src/Roman.php
+++ src/Roman.php
@@ -10,7 +10,6 @@ class Roman
             1 => 'I',
             5 => 'V',
             10 => 'X',
-            50 => 'L',
         ];
 
         if (true === isset($map[$number])) {
--- test/RomanTest.php
+++ test/RomanTest.php
@@ -21,11 +21,6 @@ class RomanTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('X', Roman::of(10));
     }
 
-    public function test_50_should_be_L()
-    {
-        $this->assertEquals('L', Roman::of(50));
-    }
-
     public function test_2_should_be_II()
     {
         $this->assertEquals('II', Roman::of(2));
