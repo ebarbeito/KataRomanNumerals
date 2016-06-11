a40f10130fcd476b776e0012c9513b7dad9da5d5
Enrique Barbeito García <ebarbeito@gmail.com>
2016-06-11 13:16:18 +0200

    Refactor: simplify all current test methods to only one

--- test/RomanTest.php
+++ test/RomanTest.php
@@ -6,53 +6,12 @@ use KataRomanNumerals\Roman;
 
 class RomanTest extends \PHPUnit_Framework_TestCase
 {
-    public function test_1_should_be_I()
+    public function test_arabic_to_roman()
     {
-        $this->assertEquals('I', Roman::of(1));
-    }
+        $data = file_get_contents(sprintf('%s/cases.json', __DIR__));
 
-    public function test_5_should_be_V()
-    {
-        $this->assertEquals('V', Roman::of(5));
-    }
-
-    public function test_10_should_be_X()
-    {
-        $this->assertEquals('X', Roman::of(10));
-    }
-
-    public function test_2_should_be_II()
-    {
-        $this->assertEquals('II', Roman::of(2));
-    }
-
-    public function test_3_should_be_III()
-    {
-        $this->assertEquals('III', Roman::of(3));
-    }
-
-    public function test_4_should_be_IV()
-    {
-        $this->assertEquals('IV', Roman::of(4));
-    }
-
-    public function test_6_should_be_VI()
-    {
-        $this->assertEquals('VI', Roman::of(6));
-    }
-
-    public function test_7_should_be_VII()
-    {
-        $this->assertEquals('VII', Roman::of(7));
-    }
-
-    public function test_8_should_be_VIII()
-    {
-        $this->assertEquals('VIII', Roman::of(8));
-    }
-
-    public function test_9_should_be_IX()
-    {
-        $this->assertEquals('IX', Roman::of(9));
+        foreach (json_decode($data) as $symbol => $number) {
+            $this->assertEquals($symbol, Roman::of($number));
+        }
     }
 }
