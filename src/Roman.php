c24a7c9055080a2b2e19fe04a90727409e07f1e7
Enrique Barbeito García <ebarbeito@gmail.com>
2016-06-11 13:03:48 +0200

    Refactor: a map of roman numbers can contain all equivalences you want

--- src/Roman.php
+++ src/Roman.php
@@ -4,15 +4,17 @@ namespace KataRomanNumerals;
 
 class Roman
 {
+    private static $equivalences = [
+      1 => 'I',
+      4 => 'IV',
+      5 => 'V',
+      6 => 'VI',
+      10 => 'X',
+    ];
+
     public static function of ($number)
     {
-        $map = [
-            1 => 'I',
-            4 => 'IV',
-            5 => 'V',
-            6 => 'VI',
-            10 => 'X',
-        ];
+        $map = self::$equivalences;
 
         if (true === isset($map[$number])) {
             return $map[$number];
