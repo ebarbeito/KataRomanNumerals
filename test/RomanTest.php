cef0346fc659568d050677b4c9e2acc23a65973f
Enrique Barbeito García <ebarbeito@gmail.com>
2016-06-14 21:11:15 +0200

    Refactor: tests cleanup

--- test/RomanTest.php
+++ test/RomanTest.php
@@ -23,43 +23,10 @@ class RomanTest extends \PHPUnit_Framework_TestCase
           : [];
     }
 
-    public function test_arabic_to_roman()
+    public function test_number_to_roman()
     {
         foreach ($this->cases as $symbol => $value) {
             $this->assertEquals($symbol, Roman::of($value));
         }
-    }
-
-    public function test_12_should_be_XII()
-    {
-        $this->assertEquals('XII', Roman::of(12));
-    }
-
-    public function test_13_should_be_XIII()
-    {
-        $this->assertEquals('XIII', Roman::of(13));
-    }
-
-    public function test_14_should_be_XIV()
-    {
-        $this->assertEquals('XIV', Roman::of(14));
-    }
-
-    public function test_15_should_be_XV()
-    {
-        $this->assertEquals('XV', Roman::of(15));
-    }
-
-    public function test_16_should_be_XVI()
-    {
-        $this->assertEquals('XVI', Roman::of(16));
-    }
-
-    public function test_all_remaining_simple_symbols_in_map()
-    {
-        $this->assertEquals('L', Roman::of(50));
-        $this->assertEquals('C', Roman::of(100));
-        $this->assertEquals('D', Roman::of(500));
-        $this->assertEquals('M', Roman::of(1000));
     }
 }
