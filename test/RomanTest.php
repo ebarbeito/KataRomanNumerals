5520307265a783b018a4ac5fae5f535977d4ada9
Enrique Barbeito García <ebarbeito@gmail.com>
2016-06-14 21:43:58 +0200

    Refactor: tests cleanup

--- test/RomanTest.php
+++ test/RomanTest.php
@@ -29,24 +29,4 @@ class RomanTest extends \PHPUnit_Framework_TestCase
             $this->assertEquals($symbol, Roman::of($value));
         }
     }
-
-    public function test_60_should_be_LX()
-    {
-        $this->assertEquals('LX', Roman::of(60));
-    }
-
-    public function test_90_should_be_LX()
-    {
-        $this->assertEquals('XC', Roman::of(90));
-    }
-
-    public function test_400_should_be_CD()
-    {
-        $this->assertEquals('CD', Roman::of(400));
-    }
-
-    public function test_900_should_be_CM()
-    {
-        $this->assertEquals('CM', Roman::of(900));
-    }
 }
