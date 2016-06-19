e8e09ca10d637a9b5c289eadf04f709184f6806c
Enrique Barbeito García <ebarbeito@gmail.com>
2016-06-19 17:15:30 +0200

    Test simplification asserting the json data suite cases with encode and decode

--- test/ConverterTest.php
+++ test/ConverterTest.php
@@ -34,7 +34,7 @@ class ConverterTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * Test the JSON data suite cases.
+     * Test encode method with JSON data suite cases.
      */
     public function test_number_to_roman()
     {
@@ -43,23 +43,13 @@ class ConverterTest extends \PHPUnit_Framework_TestCase
         }
     }
 
-    public function test_I_should_be_1()
+    /**
+     * Test decode method with JSON data suite cases.
+     */
+    public function test_roman_to_number()
     {
-        $this->assertEquals(1, $this->converter->decode('I'));
-    }
-
-    public function test_IV_should_be_4()
-    {
-        $this->assertEquals(4, $this->converter->decode('IV'));
-    }
-
-    public function test_III_should_be_3()
-    {
-        $this->assertEquals(3, $this->converter->decode('III'));
-    }
-
-    public function test_VIII_should_be_8()
-    {
-        $this->assertEquals(8, $this->converter->decode('VIII'));
+        foreach ($this->cases as $symbol => $value) {
+            $this->assertEquals($value, $this->converter->decode($symbol));
+        }
     }
 }
